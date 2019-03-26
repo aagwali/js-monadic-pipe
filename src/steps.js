aa7e049d2d7028060dd2d73df4a5b84a20cabe42
aagwali <aagwali@vente-privee.com>
2019-03-26 01:02:24 +0100

    infix and split in parts

--- src/steps.js
+++ src/steps.js
@@ -1,45 +1,27 @@
 //#region Imports
-import {
-  path,
-  map,
-  pipe,
-  replace,
-  filter,
-  equals,
-  head,
-  chain,
-  split,
-  concat,
-  nth
-} from 'ramda'
-import * as Monad from './monads-utils'
-const Maybe = require('folktale/maybe')
-const Result = require('folktale/result')
-const Future = require('folktale/concurrency/future')
+import { path, map, pipe, replace, filter, equals, head, chain, split, concat, nth } from 'ramda'
+import * as M from './monads-utils'
 import fs from 'fs'
 //#endregion
 
-export const parseContent = initialInput =>
-  Result.fromMaybe(
-    Maybe.fromNullable(path(['content'], initialInput)),
-    'Required -content- key is missing on initial input.'
-  ).fold(Future.rejected, Future.of)
+const transformContent = replace('anythingUseful', './src')
 
-export const fsAccess = pipe(
-  replace('anythingUseful', './src'),
-  Monad.futureFromCallback(fs.readdir),
-  map(
-    pipe(
-      filter(equals('monads-utils.js')),
-      head,
-      concat('./src/')
-    )
-  ),
-  chain(Monad.futureFromCallback(fs.readFile, 'utf8')),
-  map(
-    pipe(
-      split('\n'),
-      nth(1)
-    )
-  )
+const readdirAsync = M.futureFromCallback(fs.readdir)
+
+const readFileAsync = M.futureFromCallback(fs.readFile, 'utf8')
+
+const makeFilePath = filter(equals('monads-utils.js')) & head & concat('./src/') // head is failable cf.15
+
+const readLine = line => file => file |> split('\n') & nth(line)
+
+export const tryParseContent = M.futureFromMaybe('Content key is missing')(path(['content']))
+
+export const tryReadLine = x => M.futureFromMaybe('Selected line does not exists')(readLine(x))
+
+export const readUtilsFile = pipe(
+    transformContent,
+    readdirAsync,
+    map(makeFilePath),
+    chain(readFileAsync),
+    chain(tryReadLine(15))
 )
